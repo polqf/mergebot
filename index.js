5bb028198b2fbb4f58346dd7cb65598ea050c36f
Pol Quintana <pol.quintana1@gmail.com>
2017-10-02 20:03:58 +0200

    Add comment when missing PR Review

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+	extended: true
 }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
 app.post('/hook', function(request, response) {
